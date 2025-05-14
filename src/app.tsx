dbdc2693c98931ce018e9e2eabc517d7b556bfb5
OliWebDevO <oliver-vdb@hotmail.com>
2025-05-14 12:15:11 +0200

    init

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <main>
+        <div className="flex-center">
+            <h1 className="text-white-50">Welcome to my 3D Portfolio</h1>
+        </div>
+    </main>
+  )
+}
+
+export default App
